401f3bc7eb53def362a155f3acde90473b60d5d0
josrric <jreveron95@gmail.com>
2017-04-19 00:34:55 -0400

    creacion de controladores

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,86 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/* RUTAS PUBLICAS
+==========================================*/
+Route::group(['prefix' => '/'], function(){
+
+	Route::get('', function () {
+    	return view('main');
+	});
+
+	Route::group(['prefix' => 'inf.institucional/'], function(){
+
+		Route::get('historia', ['as' => 'inf.institucional.historia',function() {
+			return view('historia');
+		}]);
+
+		Route::get('mision-y-vision', ['as' => 'inf.institucional.misionyvision', function() {
+			return view('misionyvision');
+		}]);
+
+		Route::get('objetivos', ['as' => 'inf.institucional.objetivos', function() {
+			return view('objetivos');
+		}]);
+	});
+
+	Route::group(['prefix' => 'comisiones-permanentes/'], function(){
+
+		Route::get('contraloria-presupuesto', ['as' => 'contraloria.presupuesto', function() {
+			return view('comisionespermanentes.contraloriapresupuesto');
+		}]);
+		Route::get('asuntos-legislativos', ['as' => 'asuntoslegislativos', function() {
+			return view('comisionespermanentes.asuntoslegislativos');
+		}]);
+		Route::get('transporte-vialidad-y-comunicaciones', ['as' => 'transporte.vialidad', function() {
+			return view('comisionespermanentes.transportevialidad');
+		}]);
+		Route::get('ejido-terreno-municipales', ['as' => 'ejido.terreno', function() {
+			return view('comisionespermanentes.ejidoterreno');
+		}]);
+		Route::get('arquitectura-urbanismo-y-construccion', ['as' => 'arquitectura.urbanismo', function() {
+			return view('comisionespermanentes.arquitecturaurbanismo');
+		}]);
+		Route::get('servicios-publicos-salud-y-ambiente', ['as' => 'serviciospublicos.salud', function() {
+			return view('comisionespermanentes.serviciospublicossalud');
+		}]);
+		Route::get('participacion-comunitaria', ['as' => 'participacioncomunitaria', function() {
+			return view('comisionespermanentes.participacioncomunitaria');
+		}]);
+		Route::get('infraestructura-educacion-y-cultura', ['as' => 'infraestructura.educacion', function() {
+			return view('comisionespermanentes.infraestructuraeducacion');
+		}]);
+		Route::get('deporte-recreacion-y-turismo', ['as' => 'deporte.recreacion', function() {
+			return view('comisionespermanentes.deporterecreacion');
+		}]);
+		Route::get('ordinaria-y-especial', ['as' => 'ordinaria.especial', function() {
+			return view('comisionespermanentes.ordinariaespecial');
+		}]);
+	});
+	
+	Route::get('ordenanzas',['as' => 'ordenanzas',function() {
+		return view('ordenanza');
+	}]);
+});
+
+/* RUTAS DE ADMINISTRADOR
+==========================================*/
+Route::group([ 'prefix' => 'admin'], function(){
+
+	Route::get('', function(){
+		return view('admin.plantilla.main');
+	});
+
+	Route::get('access', ['as' => 'acceso', function() {
+		return view('admin.plantilla.access');
+	}]);
+
+	Route::resource('users','UsersController');
+	Route::get('users/{id}/destroy', [
+		'uses' => 'UsersController@destroy',
+		'as'   => 'admin.users.destroy'
+		]);
+
+	Route::resource('publicaciones', 'PublicacionsController');
+	Route::resource('ordenanza', 'OrdenanzasController');
 });
