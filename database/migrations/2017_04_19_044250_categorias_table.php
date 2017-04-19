9c1f5a4ea3eb312e4652e101a292f48e1b140bd6
josrric <jreveron95@gmail.com>
2017-04-19 00:55:15 -0400

    migraciones creadas y completadas

--- database/migrations/2017_04_19_044250_categorias_table.php
+++ database/migrations/2017_04_19_044250_categorias_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CategoriasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('type', ['admin','member'])->default('member');
-            $table->rememberToken();
+            $table->string('nombre');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('categorias');
     }
 }
--- database/migrations/2017_04_19_044446_ordenanzas_table.php
+++ database/migrations/2017_04_19_044446_ordenanzas_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class OrdenanzasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ordenanzas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('type', ['admin','member'])->default('member');
-            $table->rememberToken();
+            $table->integer('año');
+            $table->string('titulo');
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('ordenanzas');
     }
 }
